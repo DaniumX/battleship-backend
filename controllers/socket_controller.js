5e627effe294718022de779aa5115643f5d08972
Daniel <94713071+DaniumX@users.noreply.github.com>
2022-06-01 00:30:42 +0200

    added handler for player joins

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -1,5 +1,39 @@
 const debug = require("debug")("battleship:socket_controller");
 let io = null;
+let players = [];
+
+const handlePlayerJoined = function (user) {
+  debug(`Player ${user} joined game`);
+
+  if (!players.length) {
+    const userOne = {
+      id: this.id,
+      room: this.id,
+      username: user,
+      isPlayersTurn: true,
+    };
+    this.join(userOne.room);
+    players.push(userOne);
+    io.to(userOne.room).emit("users:profiles", players);
+    return;
+  }
+  if (players.length) {
+    const userTwo = {
+      id: this.id,
+      room: players[0].room,
+      username: user,
+      isPlayersTurn: true,
+    };
+    this.join(userTwo.room);
+    players.push(userTwo);
+    io.to(playerTwo.room).emit("users:profiles", players);
+    return;
+  }
+
+  this.emit("lobby:full");
+  delete this.id;
+  return;
+};
 
 const handleDisconnect = function () {
   debug(`Client ${this.id} disconnected :(`);
